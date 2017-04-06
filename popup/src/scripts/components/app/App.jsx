4aba4fb006e70b3a093b8e7fc0c38cda21d3a4f8
Marianna Shvartsapel <marianna.shvartsapel@gmail.com>
2017-04-06 10:06:45 -0400

    modified colors, added Card component

--- popup/src/scripts/components/app/App.jsx
+++ popup/src/scripts/components/app/App.jsx
@@ -5,9 +5,9 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import AppBar from 'material-ui/AppBar';
-import RaisedButton from 'material-ui/RaisedButton';
 import TimePicker from 'material-ui/TimePicker';
-import {redA400} from 'material-ui/styles/colors';
+import {Card, CardText} from 'material-ui/Card';
+import {pink700, grey400, grey700} from 'material-ui/styles/colors';
 
 
 
@@ -18,7 +18,16 @@ import {redA400} from 'material-ui/styles/colors';
 // More on Colors: http://www.material-ui.com/#/customization/colors
 const muiTheme = getMuiTheme({
   palette: {
-    primary1Color: redA400,
+    primary1Color: pink700,
+    primary2Color: pink700,
+    primary3Color: pink700,
+    accent1Color: grey400,
+    accent2Color: grey400,
+    accent3Color: grey400,
+    textColor: grey700,
+    alternateTextColor: grey700,
+    canvasColor: grey400,
+    pickerHeaderColor: pink700
   },
   appBar: {
     height: 50,
@@ -66,30 +75,29 @@ class App extends Component {
       now: new Date(),
       timeLeft: this.props.time - this.state.now
     });
-    
-
   }
 
-millisToMinutesAndSeconds(millis) {
-  var minutes = Math.floor(millis / 60000);
-  var seconds = ((millis % 60000) / 1000).toFixed(0);
-  return (seconds == 60 ? (minutes+1) + ":00" : minutes + ":" + (seconds < 10 ? "0" : "") + seconds);
-}
+  millisToMinutesAndSeconds(millis) {
+    var minutes = Math.floor(millis / 60000);
+    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    return (seconds == 60 ? (minutes+1) + ":00" : minutes + ":" + (seconds < 10 ? "0" : "") + seconds);
+  }
 
   render() {
-    console.log('props inside App', this.props)
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-        {/*<h1 style={{color: "white"}}>Hello</h1>*/}
-        {/*<RaisedButton label="Default" />*/}
+      <MuiThemeProvider muiTheme={muiTheme}>
         <div>
-          <AppBar title="Take A Moment" style={{color: redA400}} />
-          <h1 style={{color: "white"}}>When would you like to take a break?</h1>
-          <TimePicker value={this.props.time} hintText="12hr Format" onChange={this.handleChange}/>
-          <h1 style={{color: "white"}}>
-            {this.state.timeLeft && 
-            `Your break is coming up in ${this.millisToMinutesAndSeconds(this.state.timeLeft)} minutes`} 
-          </h1>
+          <AppBar title="Take A Moment" />
+            <Card>
+              <CardText>
+                <h2 style={{color: grey700}}>When would you like to take a break?</h2>
+                <TimePicker value={this.props.time} hintText="12hr Format" onChange={this.handleChange}/>
+                <h2 style={{color: grey700}}>
+                  {this.state.timeLeft && 
+                  `Your break is coming up in ${this.millisToMinutesAndSeconds(this.state.timeLeft)} minutes`} 
+                </h2>
+              </CardText>
+            </Card>
         </div>  
       </MuiThemeProvider>
     );
@@ -109,28 +117,18 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => {
-
   return {
     setTime: (time) => {
       // Date.prototype.getTime()
       //Returns the numeric value of the specified date as the number of milliseconds since January 1, 1970, 00:00:00 UTC
       //this is how it'll be stored on the state, because just assigning the tme (Date) object to the time key is not working...
       dispatch({
-      type: 'SET_TIME', 
-      time: time.getTime()
-    })
+        type: 'SET_TIME', 
+        time: time.getTime()
+      })
     }
   };
 };
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-
-/*
-      <div>
-        <h1>Hello  Marianna</h1>
-
-        {<img id="myImg" src="nasa.jpg" alt="The Universe is Vast!" />}
-
-      </div>*/
